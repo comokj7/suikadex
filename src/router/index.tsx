7ef190ba0aa86a3b8a3c2ea27643e212c19dbc3a
AKAI_SUIKA <comokj+dev@gmail.com>
2021-06-19 01:55:46 +0900

    Deploy on github pages

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
 import { PokemonDetail } from '../pages/PokemonDetail';
 import { PokemonList } from '../pages/PokemonList';
 
 export const Router: React.FC = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route path="/pokemon/:no">
           <PokemonDetail />
         </Route>
         <Route path="/pokemon">
           <PokemonList />
+        </Route>
+        <Route path="/">
+          <Redirect to="/pokemon" />
         </Route>
       </Switch>
     </BrowserRouter>
